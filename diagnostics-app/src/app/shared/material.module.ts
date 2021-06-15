e09d61cf38c735968ebbd02c668cf8ed06f3d606
Aayush Joglekar <aayushjog@gmail.com>
2021-06-15 11:18:49 +0530

    [app] Add default material toolbar

--- diagnostics-app/src/app/shared/material.module.ts
+++ diagnostics-app/src/app/shared/material.module.ts
@@ -11,9 +11,11 @@
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
 })
 export class MaterialModule {}
