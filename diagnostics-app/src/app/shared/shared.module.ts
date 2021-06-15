a56db4125976765a6e8b2820b4aa76b15a0713ce
Aayush Joglekar <aayushjog@gmail.com>
2021-06-15 11:04:37 +0530

    [app] Init material module

--- diagnostics-app/src/app/shared/shared.module.ts
+++ diagnostics-app/src/app/shared/shared.module.ts
@@ -10,11 +10,13 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HeaderComponent } from './components/header/header.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [HeaderComponent, MaterialModule],
 })
 export class SharedModule {}
