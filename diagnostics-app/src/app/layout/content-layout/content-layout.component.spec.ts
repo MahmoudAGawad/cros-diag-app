add0e6d04b8308d33424c97f2f58a6f5fccc62dc
Aayush Joglekar <aayushjog@gmail.com>
2021-06-15 16:09:20 +0530

    [app] Add file headers

--- diagnostics-app/src/app/layout/content-layout/content-layout.component.spec.ts
+++ diagnostics-app/src/app/layout/content-layout/content-layout.component.spec.ts
@@ -1,3 +1,11 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Defines tests for the Content Layout Component.
+ */
+
 import { LayoutModule } from '@angular/cdk/layout';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
--- diagnostics-app/src/app/layout/content-layout/content-layout.component.ts
+++ diagnostics-app/src/app/layout/content-layout/content-layout.component.ts
@@ -1,3 +1,13 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Defines the Content Layout Component.
+ * This is the root layout component which holds the entire app.
+ * Imported by app.module.ts
+ */
+
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
