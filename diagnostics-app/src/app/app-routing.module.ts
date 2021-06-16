8d0b5578997b7c0c94b18319c266a0184a03b8e2
Aayush Joglekar <aayushjog@gmail.com>
2021-06-16 17:36:17 +0530

    [app] Initialise AngularCLI project (#1)
    * [app] Initialise AngularCLI project
    
    - Remove boilerplate code
    - Install @angular/pwa support
    - Refresh PWA Cache on update
    - Comment out failing dummy test
    - Add @angular/material
    - Add Source Code Headers

---
+++ diagnostics-app/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Defines routes for app.module
+ * Imported by app.module.ts 
+ */
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ diagnostics-app/src/main.ts
@@ -0,0 +1,21 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview The root typescript file which bootstraps
+ * the Angular application on index.html
+ */
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
