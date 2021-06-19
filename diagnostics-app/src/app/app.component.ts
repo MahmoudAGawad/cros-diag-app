49433b9881fa4489bd8e3bb04805d762ad893e9c
Aayush Joglekar <aayushjog@gmail.com>
2021-06-19 16:05:14 +0530

    [app] Simplify theme change logic

--- diagnostics-app/src/app/app.component.ts
+++ diagnostics-app/src/app/app.component.ts
@@ -7,13 +7,23 @@
  * Imported by app.module.ts
  */
 
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  theme: string = 'dark';
+  //@ts-ignore
+  @HostBinding('class') cssClass: string;
   constructor() {}
+  ngOnInit() {
+    this.cssClass = this.theme;
+  }
+  toggleTheme() {
+    this.theme = this.theme === 'light' ? 'dark' : 'light';
+    this.cssClass = this.theme;
+  }
 }
