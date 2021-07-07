f4bf883183da3e6da73ff816bf5350fec9e51bf9
Aayush Joglekar <aayushjog@gmail.com>
2021-07-07 18:50:14 +0530

    [ext] Initialise extension

---
+++ diagnostics-extension/webpack.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/sw.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  output: {
+    filename: "sw.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
